5bd979e0117e8ea7e3fbeb1fd9ab843e6fe2e52b
RRoggia <rroggia>
2018-10-24 18:36:04 -0300

    add feedbacks to the model

--- webapp/controller/Feed.controller.js
+++ webapp/controller/Feed.controller.js
@@ -1,11 +1,31 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 	return Controller.extend("com.sap.techschool.controller.Feed", {
+		feedbackModel: null,
+
+		onInit: function(){
+			this.feedbackModel = new JSONModel(Array.from([]));
+			this.getView().setModel(this.feedbackModel);
+		},
 
 		onPostFeedback: function(oEvent){
 			var feedbackEntered = oEvent.getParameter("value");
+
+			var date = new Date(); 
+
+			var feedback = {
+				date: date.getDate(),
+				feedback: feedbackEntered
+			}
+
+			var feedbacks = JSON.parse(this.feedbackModel.getJSON());
+			feedbacks.push(feedback);
+			this.feedbackModel.setJSON(JSON.stringify(feedbacks), false);
+
+
 			console.log('feedback added', feedbackEntered);
 		}
 
